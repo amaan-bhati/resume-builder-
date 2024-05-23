47f4df6040559bcdd4e3d2c6f59dd4cad572c3eb
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-05-23 17:25:56 +0530

    initial commit

---
+++ utility/DefaultResumeData.jsx
@@ -0,0 +1,128 @@
+const DefaultResumeData = {
+  name: "Amaan Bhati",
+  position: "Web/Software Developer",
+  contactInformation: "6200658194",
+  email: "amaanbhati49@gmail.com",
+  address: "Bangalore, India",
+  profilePicture: "",
+  socialMedia: [
+    {
+      socialMedia: "Github",
+      link: "github.com/amaan-bhati",
+    },
+    {
+      socialMedia: "LinkedIn",
+      link: "linkedin.com/in/amaan-bhati",
+    },
+    {
+      socialMedia: "Website",
+      link: "https://amaanbhati.tech",
+    },
+  ],
+  summary: "Result-driven Computer Science student with a strong aptitude for development and proficiency in modern   frameworks, Eager to contribute technical skills and innovation to a dynamic software engineering role in a cutting-edge tech environment. Recognized for being a quick learner with the ability to swiftly adapt to new technology stacks.",
+  
+
+  
+
+  education: [
+    {
+      "branch": "Computer Science and Engineering",
+      "degree": "B.Tech",
+      "college":"KIIT Bhubaneswar",
+      "startYear": "oct-2021",
+      "endYear": "jul-2025"
+    },
+  ],
+  workExperience: [
+    {
+      "company": "KIIT Digital",
+      "position": "Web and Software Developer",
+      "description": "KIIT Digital is the tech backbone responsible for crafting websites and software for all university school and conference websites at KIIT University.",
+      "keyAchievements": "Worked as a Web/Software developer in order to develop and implement responsive user interfaces (UIs) and software for multiple KIIT University school and conference websites.\nCollaborated closely with KIIT Digital designers to translate UI mockups into user-friendly front-end code and optimized website UIs for improved user experience (UX) and faster loading times, adhering to best practices for web performance.",
+      "startYear": "2024-02-7",
+      "endYear": "2024-05-30"
+    },
+    {
+      "company": "MLSA KIIT",
+      "position": "Web Dev Lead",
+      "keyAchievements": " Led High-Performing Tech Team: Managed a team of 20+ designers, front-end, and back-end developers, fostering collaboration and driving project success.\n Oversaw the development of multiple high-traffic website, ensuring high functionality and scalability\n Led initiatives to build a vibrant tech community, attracting, hosting and engaging aspiring developers through various events and activities.",
+      "startYear": "2023-06-23",
+      "endYear": "Present"
+    },
+    // {
+    //   "company": "GSSOC",
+    //   "position": "Mentor",
+    //   "description": "Heller is a Software Editor specialized in logistics management.",
+    //   "keyAchievements": "Developed several automation systems - saved over $50,000 annually.\nJoined the team as #10 employee as a developer working directly with the biggest customers.\nInvolved in coding as per the requirement, delivered well ahead of time which saved project cost by 20%.\nReduced database size by nearly 50% and improved its performance by 20%.\nDelivery time of good reduced by 50%.",
+    //   "startYear": "2023-02-22",
+    //   "endYear": "2023-03-09"
+    // }
+  ],
+  projects: [
+    {
+      title: "MLSAKIIT Website",
+      link: "HTTPS://MLSAKIIT.COM",
+      description: "",
+      keyAchievements: "Pioneered High-Traffic Community Website: Led the design and development of the official MLSAKIIT website, utilizing Figma for UI/UX design and leveraging cutting-edge Next.js and Tailwind CSS frameworks. The website efficiently handles high traffic due to its robust architecture and scalability\nImplemented the idea of user dashboard and digital passes for tech events making the entry smooth and fast using Next.js when it was not yet mainstream",
+      startYear: "Mar 2023",
+      endYear: "May 2023",
+    },
+    {
+      title: "KIITFEST Website",
+      name:"KIITFEST Website",
+      link:"HTTPS://KIITFEST.ORG",
+      description: "",
+      keyAchievements: "Contributed to the development of the KIITFEST website, East India’s largest cultural-technical fest website, managing 20k+ registrations\n Collaborated with design and testing team for optimal user experience and experienced handling     high-traffic",
+      startYear: "Dec 2023",
+      endYear: "Feb 2024",
+    },
+
+  ],
+  languages: [
+    "JavaScript", "Typescript", "C++", "Java", "Python", "HTML", "CSS", "SQL"
+  ],
+  skills: [
+   
+    {
+      title: "Libraries and frameworks",
+      skills: [
+         "ReactJs", "Nextjs", "TailwindCSS", "SCSS", "ShadCN","NodeJS", "ExpressJS", "NextAuth",
+      ]
+    },
+    {
+      title: "Databases",
+      skills: [
+         "MongoDb", "MySQL", "PostgreSQL", "Supabase"
+      ]
+    },
+    
+    {
+      title: "Tools",
+      skills: [
+        "VS Code", "AWS", "Docker", "Postman",  "Figma", "Framer Motion",
+      ]
+    },
+    {
+      title: "Additional Skills",
+      skills: [
+        "Public Speaking", "Blog Writing", "Research", "Event Hosting", "Problem Solving"
+      ] 
+    },
+    {
+      title: "Academics",
+      skills: [
+        "Data Structures and Algorithms",
+        "DBMS",
+        "Computer Networks",
+        "Operating Systems",
+      ] 
+    }
+  ],
+
+  certifications: [
+    "Appreciation certificate for contributing to Diversion open source program.",
+    "GSSOC Certification for working as a mentor in their open source program.",
+  ],
+};
+
+export default DefaultResumeData;
